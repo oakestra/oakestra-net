625f7cf57dd8897a25c408a95226456885456de2
Giovanni Bartolomeo <giovanni.bartolomeo.rg@gmail.com>
2022-09-30 18:46:24 +0200

    fixed workflows (#47)

--- node-net-manager/build/install.sh
+++ node-net-manager/build/install.sh
@@ -1,11 +1,6 @@
 echo "Usage ./install.sh <architecture>"
 echo "supported architectures: amd64, arm-7"
 
-if [ "$1" == "" ]; then
-    echo "Architecture not set"
-    exit 1
-fi
-
 sudo cp ./NetManager /bin/NetManager
 sudo mkdir /etc/netmanager
 sudo cp tuncfg.json /etc/netmanager/tuncfg.json
