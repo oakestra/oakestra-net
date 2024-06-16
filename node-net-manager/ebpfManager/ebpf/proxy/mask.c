0f5f503fed1489fece1aac3d3d2f8f74d5107b3f
Ben Riegel <ben.riegel@tum.de>
2024-06-16 09:42:01 +0000

    put ip masking to another file

---
+++ node-net-manager/ebpfManager/ebpf/proxy/mask.c
@@ -0,0 +1,41 @@
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+#include <linux/if_ether.h>
+#include <linux/ip.h>
+#include <linux/ipv6.h>
+#include <linux/icmp.h>
+#include <linux/udp.h>
+#include <linux/tcp.h>
+#include <netinet/in.h>
+#include <bpf/bpf_endian.h>
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+#include <linux/pkt_cls.h>
+
+#define IPv4LEN 4
+#define IPv6LEN 16
+
+#define IPV4_SUBNET 0xC0A80100 // 192.168.1.0
+#define IPV4_MASK 0xFFFFFF00   // 255.255.255.0
+
+#define IPV6_SUBNET                                                                                    \
+    {                                                                                                  \
+        0x20, 0x01, 0x0d, 0xb8, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 \
+    }
+#define IPV6_MASK                                                                                      \
+    {                                                                                                  \
+        0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 \
+    }
+
+extern bool is_ipv4_in_network(__be32 addr) {
+    return (addr & IPV4_MASK) == (IPV4_SUBNET & IPV4_MASK);
+}
+
+extern bool is_ipv6_in_network(struct in6_addr *addr, struct in6_addr *network, struct in6_addr *mask) {
+    for (int i = 0; i < 4; i++) {
+        if ((addr->in6_u.u6_addr32[i] & mask->in6_u.u6_addr32[i]) != (network->in6_u.u6_addr32[i] & mask->in6_u.u6_addr32[i])) {
+            return false;
+        }
+    }
+    return true;
+}
--- node-net-manager/ebpfManager/ebpf/proxy/proxy.c
+++ node-net-manager/ebpfManager/ebpf/proxy/proxy.c
@@ -13,6 +13,9 @@
 #include <bpf/bpf_helpers.h>
 #include <linux/pkt_cls.h>
 
+extern bool is_ipv4_in_network(__be32 addr, __be32 network, __be32 mask);
+extern bool is_ipv6_in_network(struct in6_addr *addr, struct in6_addr *network, struct in6_addr *mask);
+
 struct bpf_map_def SEC("maps") lookup_table = {
     .type = BPF_MAP_TYPE_HASH,
     .key_size = sizeof(__u8),
@@ -24,7 +27,26 @@ SEC("classifier")
 int handle_ingress(struct __sk_buff *skb)
 {
     struct ethhdr eth;
+    struct iphdr ipv4;
+    struct ipv6hdr ipv6;
+    bool isSubInNet = false;
+    __u8 masked_ip[IPv6LEN] = {0};
+
     bpf_skb_load_bytes(skb, 0, &eth, sizeof(eth));
+
+    if (eth.h_proto == bpf_htons(ETH_P_IP)) {
+        // IPv4 packet
+        if (bpf_skb_load_bytes(skb, sizeof(eth), &ipv4, sizeof(ipv4)) < 0)
+            return TC_ACT_SHOT;
+
+        isSubNet = is_ipv4_in_network(ipv4.daddr, );
+    } else if (eth.h_proto == bpf_htons(ETH_P_IPV6)) {
+        // IPv6 packet
+        if (bpf_skb_load_bytes(skb, sizeof(eth), &ipv6, sizeof(ipv6)) < 0)
+            return TC_ACT_SHOT;
+
+
+    }
 
     if (eth.h_proto != bpf_htons(ETH_P_IP))
         return TC_ACT_UNSPEC;
