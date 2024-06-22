375cb6387a7b3a4da8642fa4dba3338107f1ccfc
Ben Riegel <ben.riegel@tum.de>
2024-06-22 22:47:07 +0000

    proxy rebuild compiles now. Still has few flaws!

--- node-net-manager/ebpfManager/ebpf/proxy/proxy.c
+++ node-net-manager/ebpfManager/ebpf/proxy/proxy.c
@@ -14,42 +14,43 @@
 #include <stdbool.h>
 #include <stddef.h>
 
+#define MAX_IPS 32 // implies that no more than 32 replicas of one service can exist
+
 struct session_key {
-    __u32 src_ip;
-    __u32 dst_ip;
-    __u16 src_port;
-    __u16 dst_port;
+    __be32 src_ip;
+    __be32 dst_service_ip;
+    __be16 src_port;
+    __be16 dst_port;
+};
+
+struct ip_list {
+    int length;
+    __be32 ips[MAX_IPS];
 };
 
 struct bpf_map_def SEC("maps/service_to_instance") service_to_instance = {
         .type = BPF_MAP_TYPE_HASH,
-        .key_size = sizeof(__u32),
-        .value_size = sizeof(__u32),
-        .max_entries = 128,
-};
-
-struct bpf_map_def SEC("maps/round_robin_state") round_robin_state = {
-        .type = BPF_MAP_TYPE_HASH,
-        .key_size = sizeof(__u32),
-        .value_size = sizeof(__u32),
+        .key_size = sizeof(__be32),
+        .value_size = sizeof(struct ip_list),
         .max_entries = 128,
 };
 
 struct bpf_map_def SEC("maps/open_sessions") open_sessions = {
         .type = BPF_MAP_TYPE_HASH,
         .key_size = sizeof(struct session_key),
-        .value_size = sizeof(__u32),
+        .value_size = sizeof(__be32),
         .max_entries = 1024,
 };
 
-int proxy(struct __sk_buff *skb){
+int outgoing_proxy(struct __sk_buff *skb){
     void *data = (void *) (long) skb->data;
     void *data_end = (void *) (long) skb->data_end;
 
     struct ethhdr *eth = data;
     struct iphdr *ip;
-    struct tcphdr *tcp;
-    struct udphdr *udp;
+
+    struct session_key key = {};
+    __u32 new_daddr;
 
     // check if enough size for ethernet header
     if ((void *) (eth + 1) > data_end) {
@@ -65,15 +66,10 @@ int proxy(struct __sk_buff *skb){
     if ((void *)(ip + 1) > data_end)
         return TC_ACT_OK;
 
-    if (ip->protocol != IPPROTO_TCP)
-        return TC_ACT_OK;
+    key.src_ip = ip->saddr;
+    key.dst_service_ip = ip->daddr;
 
-    __u32 service_ip = ip->daddr;
-    __u32 *instance_ip = bpf_map_lookup_elem(&service_to_instance, &service_ip);
-    if (!instance_ip)
-        return TC_ACT_OK;
-
-    iphdr_len = ip->ihl * 4;
+    int iphdr_len = ip->ihl * 4;
     if (iphdr_len < sizeof(*ip))
         // TODO ben, this more or less disables the use of IPv4 options
         //  but we need some kind of a bound for IHL for the ebpf verifier
@@ -81,61 +77,61 @@ int proxy(struct __sk_buff *skb){
 
     // proxy only supports TCP and UDP for now.
     if (ip->protocol == IPPROTO_TCP) {
+        struct tcphdr *tcp;
+
         tcp = (struct tcphdr *)((__u8 *)ip + iphdr_len);
         if ((void *)(tcp + 1) > data_end)
             return TC_ACT_OK;
 
-        struct sock *sk = skb->sk;
-        if (!sk)
-            return TC_ACT_OK;
-
-        __u32 *existing_instance_ip = bpf_map_lookup_elem(&open_connections, &sk);
-        if (existing_instance_ip) {
-            ip->daddr = *existing_instance_ip;
-            return TC_ACT_OK;
-        }
-
-        __u32 rr_key = 0;
-        __u32 *rr_state = bpf_map_lookup_elem(&round_robin_state, &rr_key);
-        if (!rr_state)
-            return TC_ACT_OK;
-
-        __u32 next_instance_ip = instance_ip[*rr_state];
-        *rr_state = (*rr_state + 1) % MAX_INSTANCES;
-
-        bpf_map_update_elem(&round_robin_state, &rr_key, rr_state, BPF_ANY);
-        bpf_map_update_elem(&open_connections, &sk, &next_instance_ip, BPF_ANY);
-
-        ip->daddr = next_instance_ip;
-        return TC_ACT_OK;
+        key.src_port = tcp->source;
+        key.dst_port = tcp->dest;
     } else if (ip->protocol == IPPROTO_UDP) {
-        udp = (struct udphdr *)((__u8 *)ip + (ip->ihl * 4));
+        struct udphdr *udp;
+
+        udp = (struct udphdr *)((__u8 *)ip + iphdr_len);
         if ((void *)(udp + 1) > data_end)
             return TC_ACT_OK;
 
-        __u32 rr_key = 0;
-        __u32 *rr_state = bpf_map_lookup_elem(&round_robin_state, &rr_key);
-        if (!rr_state)
-            return TC_ACT_OK;
-
-        __u32 next_instance_ip = instance_ip[*rr_state];
-        *rr_state = (*rr_state + 1) % MAX_INSTANCES;
-
-        bpf_map_update_elem(&round_robin_state, &rr_key, rr_state, BPF_ANY);
-
-        ip->daddr = next_instance_ip;
-        return TC_ACT_OK;
+        key.src_port = udp->source;
+        key.dst_port = udp->dest;
     }
+
+    __be32 *open_connection_ip = bpf_map_lookup_elem(&open_sessions, &key);
+    if (open_connection_ip) {
+        new_daddr = *open_connection_ip; // TODO ben this address could have gotten invalid in the meantime!
+        return TC_ACT_OK;
+    } else {
+        // if no open connection found, choose new server using round robin.
+        // TODO ben Implement other mechanisms than RR here
+
+        struct ip_list *ipl = bpf_map_lookup_elem(&open_sessions, &key);
+        if (!ipl) {
+            // TODO ben cache packet and trigger update to potentially find IP.
+            return TC_ACT_SHOT;
+        }
+
+        // select instance IP using RR
+        int rand_index = bpf_get_prandom_u32() % ipl->length % MAX_IPS;
+        new_daddr = ipl->ips[rand_index];
+
+        // add new 4-Tuple to our session cache
+        bpf_map_update_elem(&open_sessions, &key, &new_daddr, BPF_ANY);
+    }
+
+    // Update the IP header with the new destination address
+    ip->daddr = new_daddr;
+
+    // TODO ben recalculate checksum for L3 and L4.
 
     return TC_ACT_OK;
 }
 
 SEC("classifier")
 int handle_ingress(struct __sk_buff *skb) {
-    return proxy(skb);
+    return outgoing_proxy(skb);
 }
 
 SEC("classifier")
 int handle_egress(struct __sk_buff *skb) {
-    return proxy(skb);
+    return TC_ACT_OK;
 }
