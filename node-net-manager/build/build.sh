01cbd4a8c037c1469f5308c9447dcb1aa8e397ce
giobarty <giobarty@gmail.com>
2024-07-12 13:46:46 +0200

    fix net maanger versioning

--- node-net-manager/build/build.sh
+++ node-net-manager/build/build.sh
@@ -1,8 +1,8 @@
 version=$(git describe --tags --abbrev=0)
 
 #arm build
-env GOOS=linux GOARCH=arm GOARM=7 go build -ldflags="-X 'go_node_engine/cmd.Version=$version'" -o bin/arm-7-NetManager ../NetManager.go
+env GOOS=linux GOARCH=arm GOARM=7 go build -ldflags="-X 'NetManager/cmd.Version=$version'" -o bin/arm-7-NetManager ../NetManager.go
 
 #amd build
-env GOOS=linux GOARCH=amd64 go build -ldflags="-X 'go_node_engine/cmd.Version=$version'" -o bin/amd64-NetManager ../NetManager.go
+env GOOS=linux GOARCH=amd64 go build -ldflags="-X 'NetManager/cmd.Version=$version'" -o bin/amd64-NetManager ../NetManager.go
 
