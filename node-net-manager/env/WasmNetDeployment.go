3339e57ec775b201e012105a34eee5787acc7715
Alessandro Grassi <alessandrograssi17@gmail.com>
2025-02-24 20:28:25 +0100

    fix(wasm): improve wasm network manager implementation
    - Add dynamic IP allocation instead of hardcoded IPs
    - Add proper IPv6 support for WASM namespaces
    - Improve error handling and resource cleanup
    - Align with Container and Unikernel implementations
    - Return IPv6 address in API responses
    - Enhanced JSON response format for deploy/undeploy operations

--- node-net-manager/env/WasmNetDeployment.go
+++ node-net-manager/env/WasmNetDeployment.go
@@ -2,10 +2,12 @@ package env
 
 import (
 	"NetManager/logger"
+	"NetManager/mqtt"
 	"NetManager/network"
 	"fmt"
 	"net"
 	"os/exec"
+	"runtime/debug"
 
 	"github.com/vishvananda/netlink"
 	"github.com/vishvananda/netns"
@@ -31,115 +33,215 @@ func InitWasmDeployment(env *Environment) {
 }
 
 func (h *WasmDeploymentHandler) DeployNetwork(pid int, sname string, instancenumber int, portmapping string) (net.IP, net.IP, error) {
+	env := h.env
 	nsName := fmt.Sprintf("%s.instance.%d", sname, instancenumber)
 
-	hostVethName := fmt.Sprintf("veth-br-%d", instancenumber)
-	nsVethName := fmt.Sprintf("veth-ns-%d", instancenumber)
+	// Create a cleanup function to ensure proper cleanup on error
+	cleanup := func(veth *netlink.Veth) {
+		if veth != nil {
+			_ = netlink.LinkDel(veth)
+		}
+		exec.Command("ip", "netns", "del", nsName).Run()
+	}
 
+	// Clean any existing namespace or interfaces with same name
 	exec.Command("ip", "netns", "del", nsName).Run()
+
+	// Create veth pair with proper naming convention
+	hashedName := network.NameUniqueHash(sname, 4)
+	hostVethName := fmt.Sprintf("veth%s%s%s", "00", fmt.Sprintf("%d", instancenumber), hashedName)
+	nsVethName := fmt.Sprintf("veth%s%s%s", "01", fmt.Sprintf("%d", instancenumber), hashedName)
+
+	// Remove any existing veth with same name
 	exec.Command("ip", "link", "del", hostVethName).Run()
 
+	// Create namespace
 	cmd := exec.Command("ip", "netns", "add", nsName)
 	if err := cmd.Run(); err != nil {
 		logger.ErrorLogger().Printf("Failed to create namespace %s: %v", nsName, err)
 		return nil, nil, err
 	}
 
+	// Create veth pair with proper MTU
 	veth := &netlink.Veth{
 		LinkAttrs: netlink.LinkAttrs{
 			Name: hostVethName,
-			MTU:  1500,
+			MTU:  env.mtusize,
 		},
 		PeerName: nsVethName,
 	}
 	if err := netlink.LinkAdd(veth); err != nil {
 		logger.ErrorLogger().Printf("Failed to create veth pair: %v", err)
+		cleanup(nil)
 		return nil, nil, err
 	}
 
-	bridgeName := "br0"
+	// Use the environment bridge instead of hardcoded "br0"
+	bridgeName := env.config.HostBridgeName
 	bridge, err := netlink.LinkByName(bridgeName)
 	if err != nil {
-		la := netlink.NewLinkAttrs()
-		la.Name = bridgeName
-		bridge = &netlink.Bridge{LinkAttrs: la}
-		if err := netlink.LinkAdd(bridge); err != nil {
-			logger.ErrorLogger().Printf("Failed to create bridge: %v", err)
-			netlink.LinkDel(veth)
-			return nil, nil, err
-		}
-		if err := netlink.LinkSetUp(bridge); err != nil {
-			logger.ErrorLogger().Printf("Failed to set bridge up: %v", err)
-			netlink.LinkDel(veth)
-			return nil, nil, err
-		}
-		addr, _ := netlink.ParseAddr("10.10.0.1/24")
-		if err := netlink.AddrAdd(bridge, addr); err != nil {
-			logger.ErrorLogger().Printf("Failed to add IP to bridge: %v", err)
-			netlink.LinkDel(veth)
-			return nil, nil, err
-		}
+		logger.ErrorLogger().Printf("Failed to get bridge %s: %v", bridgeName, err)
+		cleanup(veth)
+		return nil, nil, err
 	}
 
+	// Assign peer veth to namespace
 	peerVeth, err := netlink.LinkByName(nsVethName)
 	if err != nil {
 		logger.ErrorLogger().Printf("Failed to retrieve peer veth: %v", err)
-		netlink.LinkDel(veth)
+		cleanup(veth)
 		return nil, nil, err
 	}
 	nsHandle, err := netns.GetFromName(nsName)
 	if err != nil {
 		logger.ErrorLogger().Printf("Failed to get namespace handle: %v", err)
-		netlink.LinkDel(veth)
+		cleanup(veth)
 		return nil, nil, err
 	}
 	if err := netlink.LinkSetNsFd(peerVeth, int(nsHandle)); err != nil {
 		logger.ErrorLogger().Printf("Failed to move veth into namespace: %v", err)
-		netlink.LinkDel(veth)
 		nsHandle.Close()
+		cleanup(veth)
 		return nil, nil, err
 	}
 	nsHandle.Close()
 
+	// Connect host veth to bridge
 	hostVeth, err := netlink.LinkByName(hostVethName)
 	if err != nil {
 		logger.ErrorLogger().Printf("Failed to retrieve host veth: %v", err)
-		netlink.LinkDel(veth)
+		cleanup(veth)
 		return nil, nil, err
 	}
 	if err := netlink.LinkSetMaster(hostVeth, bridge); err != nil {
 		logger.ErrorLogger().Printf("Failed to set bridge master for host veth: %v", err)
-		netlink.LinkDel(veth)
+		cleanup(veth)
 		return nil, nil, err
 	}
 	if err := netlink.LinkSetUp(hostVeth); err != nil {
 		logger.ErrorLogger().Printf("Failed to set host veth up: %v", err)
-		netlink.LinkDel(veth)
+		cleanup(veth)
 		return nil, nil, err
 	}
 
-	cmd = exec.Command("ip", "netns", "exec", nsName, "ip", "addr", "add", "10.10.0.2/24", "dev", nsVethName)
-	if err := cmd.Run(); err != nil {
-		logger.ErrorLogger().Printf("Failed to assign IP to namespace veth: %v", err)
-		netlink.LinkDel(veth)
+	// Generate dynamic IP addresses instead of hardcoded ones
+	ip, err := env.generateAddress()
+	if err != nil {
+		logger.ErrorLogger().Printf("Failed to generate IPv4 address: %v", err)
+		cleanup(veth)
 		return nil, nil, err
 	}
-	cmd = exec.Command("ip", "netns", "exec", nsName, "ip", "link", "set", nsVethName, "up")
-	if err := cmd.Run(); err != nil {
-		logger.ErrorLogger().Printf("Failed to set namespace veth up: %v", err)
-		netlink.LinkDel(veth)
+
+	// Generate IPv6 address
+	ipv6, err := env.generateIPv6Address()
+	if err != nil {
+		logger.ErrorLogger().Printf("Failed to generate IPv6 address: %v", err)
+		cleanup(veth)
+		env.freeContainerAddress(ip)
 		return nil, nil, err
 	}
+
+	// Configure IPv4 in namespace
+	if err := env.addPeerLinkNetworkByNsName(nsName, ip.String()+env.config.HostBridgeMask, nsVethName); err != nil {
+		logger.ErrorLogger().Printf("Failed to assign IPv4 to namespace veth: %v", err)
+		cleanup(veth)
+		env.freeContainerAddress(ip)
+		env.freeContainerAddress(ipv6)
+		return nil, nil, err
+	}
+
+	// Configure IPv6 in namespace
+	// Disable DAD for faster IPv6 setup
+	if err := env.execInsideNsByName(nsName, func() error {
+		cmd := exec.Command("sysctl", "-w", "net.ipv6.conf.default.accept_dad=0")
+		err := cmd.Run()
+		if err != nil {
+			return err
+		}
+		cmd = exec.Command("sysctl", "-w", "net.ipv6.conf."+nsVethName+".accept_dad=0")
+		err = cmd.Run()
+		return err
+	}); err != nil {
+		logger.ErrorLogger().Printf("Failed to disable DAD: %v", err)
+	}
+
+	if err := env.addPeerLinkNetworkByNsName(nsName, ipv6.String()+env.config.HostBridgeIPv6Prefix, nsVethName); err != nil {
+		logger.ErrorLogger().Printf("Failed to assign IPv6 to namespace veth: %v", err)
+		cleanup(veth)
+		env.freeContainerAddress(ip)
+		env.freeContainerAddress(ipv6)
+		return nil, nil, err
+	}
+
+	// Setup loopback interface
 	cmd = exec.Command("ip", "netns", "exec", nsName, "ip", "link", "set", "lo", "up")
 	if err := cmd.Run(); err != nil {
 		logger.ErrorLogger().Printf("Failed to set loopback up in namespace: %v", err)
-		netlink.LinkDel(veth)
+		cleanup(veth)
+		env.freeContainerAddress(ip)
+		env.freeContainerAddress(ipv6)
 		return nil, nil, err
 	}
 
-	logger.DebugLogger().Printf("Successful network creation for WASM namespace %s", nsName)
-	ip, _, _ := net.ParseCIDR("10.10.0.2/24")
-	return ip, nil, nil
+	// Add default routes
+	cmd = exec.Command("ip", "netns", "exec", nsName, "ip", "route", "add", "default", "via", env.config.HostBridgeIP)
+	if err := cmd.Run(); err != nil {
+		logger.ErrorLogger().Printf("Failed to add default IPv4 route: %v", err)
+		cleanup(veth)
+		env.freeContainerAddress(ip)
+		env.freeContainerAddress(ipv6)
+		return nil, nil, err
+	}
+
+	cmd = exec.Command("ip", "netns", "exec", nsName, "ip", "-6", "route", "add", "default", "via", env.config.HostBridgeIPv6)
+	if err := cmd.Run(); err != nil {
+		logger.ErrorLogger().Printf("Failed to add default IPv6 route: %v", err)
+		// Continue anyway as IPv6 is optional
+	}
+
+	// Set firewall rules
+	if err = env.setVethFirewallRules(hostVethName); err != nil {
+		logger.ErrorLogger().Printf("Failed to set firewall rules: %v", err)
+		cleanup(veth)
+		env.freeContainerAddress(ip)
+		env.freeContainerAddress(ipv6)
+		return nil, nil, err
+	}
+
+	// Manage ports
+	if err = network.ManageContainerPorts(ip, portmapping, network.OpenPorts); err != nil {
+		logger.ErrorLogger().Printf("Error in ManageContainerPorts v4: %v", err)
+		debug.PrintStack()
+		cleanup(veth)
+		env.freeContainerAddress(ip)
+		env.freeContainerAddress(ipv6)
+		return nil, nil, err
+	}
+
+	if err = network.ManageContainerPorts(ipv6, portmapping, network.OpenPorts); err != nil {
+		logger.ErrorLogger().Printf("Error in ManageContainerPorts v6: %v", err)
+		debug.PrintStack()
+		cleanup(veth)
+		env.freeContainerAddress(ip)
+		env.freeContainerAddress(ipv6)
+		return nil, nil, err
+	}
+
+	// Register in deployedServices
+	env.deployedServicesLock.Lock()
+	env.deployedServices[nsName] = service{
+		ip:          ip,
+		ipv6:        ipv6,
+		sname:       sname,
+		portmapping: portmapping,
+		veth:        veth,
+	}
+	env.deployedServicesLock.Unlock()
+
+	env.BookVethNumber()
+	
+	logger.DebugLogger().Printf("Successful network creation for WASM namespace %s with IP %s, IPv6 %s", nsName, ip.String(), ipv6.String())
+	return ip, ipv6, nil
 }
 
 func (env *Environment) DeleteWasmNamespace(sname string, instance int) {
@@ -155,8 +257,15 @@ func (env *Environment) DeleteWasmNamespace(sname string, instance int) {
 		delete(env.deployedServices, nsName)
 		env.deployedServicesLock.Unlock()
 		env.freeContainerAddress(s.ip)
+		env.freeContainerAddress(s.ipv6)
 		_ = network.ManageContainerPorts(s.ip, s.portmapping, network.ClosePorts)
+		_ = network.ManageContainerPorts(s.ipv6, s.portmapping, network.ClosePorts)
 		_ = netlink.LinkDel(s.veth)
 		exec.Command("ip", "netns", "del", nsName).Run()
+		
+		// if no interest registered delete all remaining info about the service
+		if !mqtt.MqttIsInterestRegistered(sname) {
+			env.RemoveServiceEntries(sname)
+		}
 	}
 }
--- node-net-manager/handlers/WasmManager.go
+++ node-net-manager/handlers/WasmManager.go
@@ -4,6 +4,7 @@ import (
 	"NetManager/env"
 	"NetManager/logger"
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -54,7 +55,8 @@ Response JSON:
 
 	{
 		"serviceName": "example-wasm-service",
-		"nsAddress":   "10.10.0.2"
+		"nsAddress":   "10.20.0.2",
+		"nsAddressV6": "fd00:1:2:3::2"
 	}
 */
 func (m *WasmManager) DeployWasmNamespace(writer http.ResponseWriter, request *http.Request) {
@@ -66,11 +68,25 @@ func (m *WasmManager) DeployWasmNamespace(writer http.ResponseWriter, request *h
 		return
 	}
 
-	reqBody, _ := io.ReadAll(request.Body)
+	reqBody, err := io.ReadAll(request.Body)
+	if err != nil {
+		logger.ErrorLogger().Printf("Failed to read request body: %v", err)
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	
 	log.Printf("ReqBody received: %s", reqBody)
 	var requestStruct ContainerDeployTask
-	err := json.Unmarshal(reqBody, &requestStruct)
+	err = json.Unmarshal(reqBody, &requestStruct)
 	if err != nil {
+		logger.ErrorLogger().Printf("Failed to unmarshal request: %v", err)
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// Validate required fields
+	if requestStruct.ServiceName == "" {
+		logger.ErrorLogger().Printf("Missing required field: serviceName")
 		writer.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -87,13 +103,23 @@ func (m *WasmManager) DeployWasmNamespace(writer http.ResponseWriter, request *h
 
 	result := <-requestStruct.Finish
 	if result.Err != nil {
+		logger.ErrorLogger().Printf("Failed to deploy WASM namespace: %v", result.Err)
 		writer.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
-	response := DeployResponse{
+	// Create response with both IPv4 and IPv6 addresses
+	response := struct {
+		ServiceName string `json:"serviceName"`
+		NsAddress   string `json:"nsAddress"`
+		NsAddressV6 string `json:"nsAddressV6,omitempty"`
+	}{
 		ServiceName: requestStruct.ServiceName,
 		NsAddress:   result.IP.String(),
+	}
+	
+	if result.IPv6 != nil {
+		response.NsAddressV6 = result.IPv6.String()
 	}
 
 	logger.InfoLogger().Println("Response to /wasm/deploy: ", response)
@@ -101,6 +127,7 @@ func (m *WasmManager) DeployWasmNamespace(writer http.ResponseWriter, request *h
 	writer.Header().Set("Content-Type", "application/json")
 	err = json.NewEncoder(writer).Encode(response)
 	if err != nil {
+		logger.ErrorLogger().Printf("Failed to encode response: %v", err)
 		writer.WriteHeader(http.StatusInternalServerError)
 	}
 }
@@ -127,15 +154,44 @@ func (m *WasmManager) DeleteWasmNamespace(writer http.ResponseWriter, request *h
 		return
 	}
 
-	reqBody, _ := io.ReadAll(request.Body)
-	var requestStruct undeployRequest
-	err := json.Unmarshal(reqBody, &requestStruct)
+	reqBody, err := io.ReadAll(request.Body)
 	if err != nil {
+		logger.ErrorLogger().Printf("Failed to read request body: %v", err)
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	
+	var requestStruct undeployRequest
+	err = json.Unmarshal(reqBody, &requestStruct)
+	if err != nil {
+		logger.ErrorLogger().Printf("Failed to unmarshal request: %v", err)
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// Validate required fields
+	if requestStruct.Servicename == "" {
+		logger.ErrorLogger().Printf("Missing required field: serviceName")
 		writer.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
 	log.Println("WASM undeploy request:", requestStruct)
 	m.Env.DeleteWasmNamespace(requestStruct.Servicename, requestStruct.Instancenumber)
+	
+	// Return success response
+	writer.Header().Set("Content-Type", "application/json")
 	writer.WriteHeader(http.StatusOK)
+	response := struct {
+		Success bool   `json:"success"`
+		Message string `json:"message"`
+	}{
+		Success: true,
+		Message: fmt.Sprintf("Successfully removed WASM namespace for service %s instance %d", 
+			requestStruct.Servicename, requestStruct.Instancenumber),
+	}
+	
+	if err := json.NewEncoder(writer).Encode(response); err != nil {
+		logger.ErrorLogger().Printf("Failed to encode response: %v", err)
+	}
 }
