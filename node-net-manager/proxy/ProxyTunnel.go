be5baa226a7e6806a64e06ff477f6d755b9524ad
Matthew Humphreys <ge65yay@tum.de>
2025-08-21 12:47:58 +0200

    debug logging
    Signed-off-by: Matthew Humphreys <ge65yay@tum.de>

--- node-net-manager/proxy/ProxyTunnel.go
+++ node-net-manager/proxy/ProxyTunnel.go
@@ -350,9 +350,6 @@ func (proxy *GoProxyTunnel) forward(dstHost net.IP, dstPort int, packet gopacket
 		return
 	}
 
-	// Check udp channel buffer to avoid creating a new channel
-	proxy.udpwrite.Lock()
-
 	// Format hoststring with [] if ipv6
 	var hoststring string
 	if dstHost.To4() != nil {
@@ -361,6 +358,8 @@ func (proxy *GoProxyTunnel) forward(dstHost net.IP, dstPort int, packet gopacket
 		hoststring = fmt.Sprintf("[%s]:%v", dstHost, dstPort)
 	}
 
+	// Check udp channel buffer to avoid creating a new channel
+	proxy.udpwrite.Lock()
 	con, exist := proxy.connectionBuffer[hoststring]
 	proxy.udpwrite.Unlock()
 	// TODO: flush connection buffer by time to time
