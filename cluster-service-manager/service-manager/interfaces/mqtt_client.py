b56ed875b6c27f70aa9bd6e976a82e0a9c551af1
Matthew Humphreys <ge65yay@tum.de>
2025-09-09 13:31:52 +0200

    more logging
    Signed-off-by: Matthew Humphreys <ge65yay@tum.de>

--- cluster-service-manager/service-manager/interfaces/mqtt_client.py
+++ cluster-service-manager/service-manager/interfaces/mqtt_client.py
@@ -151,7 +151,9 @@ def _nattraversal_handler(client_id, payload):
     app.logger.debug("Received nat traversal request with payload: %s", payload.get("dst"))
     # forward request to relevant nodes
     dst = payload.get("dst").rsplit(':', 1)
+    app.loggerr.debug("Looking for worker with host %s and port %s", dst[0], dst[1])
     dstId = mongo_find_worker_id_by_host_ip_and_port(dst[0], dst[1])
+    app.logger.debug("Found dst id: %s", dstId)
 
     # find ip and port of src
     ip, port = mongo_find_worker_ip_and_port_by_id(client_id)
